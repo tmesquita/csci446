3d1e43ad4c23c406cdf3ff72a2849668016c0ef5
Thomas Mesquita <tmesquit@mines.edu>
2012-01-17 13:08:59 -0700

    level 4 completed

--- project02/rubywarrior/r41b0wk4t-beginner/player.rb
+++ project02/rubywarrior/r41b0wk4t-beginner/player.rb
@@ -1,13 +1,19 @@
 class Player
+  @health
   def play_turn(warrior)
     unless warrior.feel.empty?
     	warrior.attack!
     else
     	if warrior.health < 20
-    		warrior.rest!
+    		unless @health > warrior.health
+    			warrior.rest!
+    		else
+    			warrior.walk!
+    		end
 	    else
 		    warrior.walk!
 		end
     end
+    @health = warrior.health
   end
 end
