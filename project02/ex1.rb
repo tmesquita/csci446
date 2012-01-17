613b542f5656f4ca0ee3561911547f3b79382a72
Thomas Mesquita <tmesquit@mines.edu>
2012-01-17 12:40:51 -0700

    Completed exercise1

---
+++ project02/ex1.rb
@@ -0,0 +1,12 @@
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+#puts "Yay! Priniting."
+#puts "I'd much rather you 'not'"
+#puts 'I "said" do not touch this.'
+puts "Adding another awesome line to print"
+
+####
+#A pound sign comments the line
+####
