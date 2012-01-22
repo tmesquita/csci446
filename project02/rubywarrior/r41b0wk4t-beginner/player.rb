09345f9d46539602919d0721f63f821f5c75678b
Thomas Mesquita <tmesquit@mines.edu>
2012-01-22 14:13:45 -0700

    passed level 6(forgot to commit) and passed level 7

--- project02/rubywarrior/r41b0wk4t-beginner/player.rb
+++ project02/rubywarrior/r41b0wk4t-beginner/player.rb
@@ -1,23 +1,61 @@
 class Player
   @health
+  @@backward = false
+  @@charge = false
+  @@turn = false
+
   def play_turn(warrior)
-    unless warrior.feel.empty?
-    	if warrior.feel.captive?
-    		warrior.rescue!
+  	if warrior.feel.wall?
+  		@@turn = true
+  	#elsif warrior.feel(:backward).wall?
+  	#	@@backward = false
+  	end
+
+    if @@backward
+    	unless warrior.feel(:backward).empty?
+	    	if warrior.feel(:backward).captive?
+	    		warrior.rescue!(:backward)
+	    	else
+		    	warrior.attack!(:backward)
+		    end
     	else
-	    	warrior.attack!
-	    end
+	    	if warrior.health < 20
+	    		unless @health > warrior.health
+	    			warrior.rest!
+	    		else
+	    			warrior.walk!(:backward)
+	    		end
+		    else
+			    warrior.walk!(:backward)
+			end
+    	end
+    	
     else
-    	if warrior.health < 20
-    		unless @health > warrior.health
-    			warrior.rest!
-    		else
-    			warrior.walk!
-    		end
+    	if @@turn
+    		warrior.pivot!
+    		@@turn = false
+		elsif !warrior.feel.empty?
+	    	if warrior.feel.captive?
+	    		warrior.rescue!
+	    	else
+		    	warrior.attack!
+		    end
 	    else
-		    warrior.walk!
-		end
-    end
+	    	if @health > warrior.health
+	    		if warrior.health <= 8
+	    			warrior.walk!(:backward)
+	    		else
+	    			warrior.walk!
+	    		end
+	    	else
+	    		if warrior.health < 15
+	    			warrior.rest!
+	    		else
+	    			warrior.walk!
+	    		end
+	    	end
+	    end
+	end
     @health = warrior.health
   end
 end
