a278222c4fc00725cd873701e75cd08927f12d10
Thomas Mesquita <tmesquit@mines.edu>
2012-01-24 13:47:43 -0700

    finished chapter 6, seeded db, and added depot.css

--- project03/depot/db/migrate/20120124200152_create_products.rb
+++ project03/depot/db/migrate/20120124200152_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string : title
+      t.string :title
       t.text :description
       t.string :image_url
-      t.decimal :price
+      t.decimal :price, :precision => 8, :scale => 2
 
       t.timestamps
     end
--- project03/depot/db/seeds.rb
+++ project03/depot/db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+Product.delete_all
+
+Product.create(:title => 'Beer n Sammich Machine',
+  :description => 
+    %{The last machine you'll ever need to buy},
+  :image_url =>   '',    
+  :price => 1500.00)
+# . . .
+
+Product.create(:title => 'Tardis',
+  :description =>
+    %{If your beer n sammich machine breaks, you can use this to go
+	    find another one},
+  :image_url => '',
+  :price => 49.99)
+# . . .
+
+Product.create(:title => 'ShamWOW',
+  :description => 
+    %{How else are you goint to keep your beer n sammich machine clean??},
+  :image_url => '',
+  :price => 9.99)
