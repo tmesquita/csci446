c161b953ecd0b7aa911a8654e3f056796602fa34
Thomas Mesquita <tmesquit@mines.edu>
2012-02-03 19:00:06 -0700

    Finished store controller (chapter 8)

--- project03/depot/db/schema.rb
+++ project03/depot/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120124200152) do
+ActiveRecord::Schema.define(:version => 20120204015720) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
--- project03/depot/db/seeds.rb
+++ project03/depot/db/seeds.rb
@@ -3,7 +3,7 @@ Product.delete_all
 Product.create(:title => 'Beer n Sammich Machine',
   :description => 
     %{The last machine you'll ever need to buy},
-  :image_url =>   '',    
+  :image_url =>   'beer_machine.jpg',    
   :price => 1500.00)
 # . . .
 
@@ -11,12 +11,12 @@ Product.create(:title => 'Tardis',
   :description =>
     %{If your beer n sammich machine breaks, you can use this to go
 	    find another one},
-  :image_url => '',
+  :image_url => 'tardis.jpg',
   :price => 49.99)
 # . . .
 
 Product.create(:title => 'ShamWOW',
   :description => 
     %{How else are you goint to keep your beer n sammich machine clean??},
-  :image_url => '',
+  :image_url => 'sham_wow.jpg',
   :price => 9.99)
