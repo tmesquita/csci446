fe658a1a73693db734ad31b8d6b4199e6aea52c6
Thomas Mesquita <tmesquit@mines.edu>
2012-02-04 16:28:17 -0700

    Revert "testing revert"
    This reverts commit ee53bf882ab7109a826785d3c6dafed998ad2a94.

--- project03/depot/app/controllers/carts_controller.rb
+++ project03/depot/app/controllers/carts_controller.rb
@@ -1,50 +1,89 @@
-require 'test_helper'
+class CartsController < ApplicationController
+  # GET /carts
+  # GET /carts.json
+  def index
+    @carts = Cart.all
 
-class CartsControllerTest < ActionController::TestCase
-  setup do
-    @cart = carts(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:carts)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create cart" do
-    assert_difference('Cart.count') do
-      post :create, :cart => @cart.attributes
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render :json => @carts }
     end
-
-    assert_redirected_to cart_path(assigns(:cart))
   end
 
-  test "should show cart" do
-    get :show, :id => @cart.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @cart.to_param
-    assert_response :success
-  end
-
-  test "should update cart" do
-    put :update, :id => @cart.to_param, :cart => @cart.attributes
-    assert_redirected_to cart_path(assigns(:cart))
-  end
-
-  test "should destroy cart" do
-    assert_difference('Cart.count', -1) do
-      session[:cart_id] = @cart.id
-      delete :destroy, :id => @cart.to_param
+  # GET /carts/1
+  # GET /carts/1.json
+  def show
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, :notice => 'invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render :json => @cart }
+      end
     end
+  end
 
-    assert_redirected_to store_path
+  # GET /carts/new
+  # GET /carts/new.json
+  def new
+    @cart = Cart.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render :json => @cart }
+    end
+  end
+
+  # GET /carts/1/edit
+  def edit
+    @cart = Cart.find(params[:id])
+  end
+
+  # POST /carts
+  # POST /carts.json
+  def create
+    @cart = Cart.new(params[:cart])
+
+    respond_to do |format|
+      if @cart.save
+        format.html { redirect_to @cart, :notice => 'Cart was successfully created.' }
+        format.json { render :json => @cart, :status => :created, :location => @cart }
+      else
+        format.html { render :action => "new" }
+        format.json { render :json => @cart.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /carts/1
+  # PUT /carts/1.json
+  def update
+    @cart = Cart.find(params[:id])
+
+    respond_to do |format|
+      if @cart.update_attributes(params[:cart])
+        format.html { redirect_to @cart, :notice => 'Cart was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render :action => "edit" }
+        format.json { render :json => @cart.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /carts/1
+  # DELETE /carts/1.json
+  def destroy
+    @cart = current_cart
+    @cart.destroy
+    session[:cart_id] = nil
+
+    respond_to do |format|
+      format.html { redirect_to (store_url, :notice => 'Your cart is currently empty') }
+      format.json { head :no_content }
+    end
   end
 end
