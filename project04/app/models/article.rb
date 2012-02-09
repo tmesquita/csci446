ed139a61bd5f91c5e4b92990cfc0e159902e145c
Thomas Mesquita <tmesquit@mines.edu>
2012-02-09 13:27:31 -0700

    added validation to article model to not allow authors name to be pat

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
 	validates :title, :body, :author, :presence => true
+	validates_exclusion_of :author, :in => %w(pat PAT Pat PAt PaT paT pAT pAt ), :message => "NO PATS ALLOWED!!! SHOO!!"
 end
