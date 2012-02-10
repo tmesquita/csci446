7d102203dc9359b8bbff697cd7fa89a7c9f20685
Thomas Mesquita <tmesquit@mines.edu>
2012-02-10 13:31:19 -0700

    Added seed data to db/seeds

--- project04/db/seeds.rb
+++ project04/db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+Article.delete_all
+
+for i in (1..25)
+	Article.create(	:id => i, 
+					:title => 'Safe and Sound',
+					:body => %{When we come down, o yeah yeah yeah 
+								We'll be dreaming safe and sound 
+								You better know why 
+								Baby that's all I got,
+
+								When we come down, o yeah yeah yeah 
+								We'll be dreaming safe and sound 
+								You better know why 
+								O Baby that's all I got, O baby that's all I got
+
+								Is This the kinda sound that you wanna hear, 
+								Is This the kinda vibe that you wanna feel, 
+								Well I'm the type of guy that you'll never fear, 
+								You can do your thing then come near,},
+					:author => 'Rebelution',
+					:date_posted => Time.now,
+					:num_edits => 1)
+end
