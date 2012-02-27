6564828defa18ee760fd84363ce52e0952b9b3af
Thomas Mesquita <tmesquit@mines.edu>
2012-02-26 23:25:21 -0700

    moved no pat validation from article model to author model

--- project05/app/models/article.rb
+++ project05/app/models/article.rb
@@ -1,15 +1,7 @@
 class Article < ActiveRecord::Base
 	validates :title, :body, :author_id, :presence => true
-	#validate :no_pats
 
 	belongs_to :author
 	
-	private
-
-		def no_pats
-			if self.author.match(/(pat)/i)
-				errors.add(:author, 'No Pats of anykind allowed here!')	
-			end
-		end
 
 end
--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,3 +1,14 @@
 class Author < ActiveRecord::Base
 	has_many :articles
+	
+	validate :no_pats
+
+	private
+
+		def no_pats
+			if self.author.match(/(pat)/i)
+				errors.add(:author, 'No Pats of anykind allowed here!')	
+			end
+		end
+
 end
