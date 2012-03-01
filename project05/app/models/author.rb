3033d581bdca5841470b91fdcb7e7b8a7c99fbf3
Thomas Mesquita <tmesquit@mines.edu>
2012-03-01 13:08:46 -0700

    added photo attachment to authors

--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,12 +1,13 @@
 class Author < ActiveRecord::Base
 	has_many :articles
-	
+	validates :name, :presence => true
 	validate :no_pats
+	has_attached_file :photo
 
 	private
 
 		def no_pats
-			if self.author.match(/(pat)/i)
+			if self.name.match(/(pat)/i)
 				errors.add(:author, 'No Pats of anykind allowed here!')	
 			end
 		end
