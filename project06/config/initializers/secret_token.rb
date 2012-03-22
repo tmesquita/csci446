42c1b723161b5443b9bde4f8553e882250992602
Thomas Mesquita <tmesquit@mines.edu>
2012-03-22 12:52:29 -0600

    Initial commit for project06 (generated rails project)

---
+++ project06/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamez::Application.config.secret_token = '782e1f898f5c115ae72276b6e8b2f678f4d155debfa69313137c95d4105f1bcedf73c774f9fa3268d196e966768e7abff1a46535e742ce2548f48e992ec17f1f'
