f3a71c5edf5ae59b19aff6e3fc0bc59878e09f31
Thomas Mesquita <tmesquit@mines.edu>
2012-03-28 22:43:50 -0600

    converted erb to haml

--- project06/config/routes.rb
+++ project06/config/routes.rb
@@ -63,7 +63,7 @@ Gamez::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#index'
+  root :to => 'games#index'
 
   # See how all your routes lay out with "rake routes"
 
