6df6dc73ef229c5367b9659d5ad177c5c3f3aebb
Thomas Mesquita <tmesquit@mines.edu>
2012-03-29 13:35:07 -0600

    added password length validation

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 	acts_as_authentic
 	has_many :games
 	belongs_to :role
+
+	validates :first_name, :last_name, :presence => true
+	validates_length_of :password, :minimum => 6
 end
