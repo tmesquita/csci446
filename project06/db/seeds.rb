2d8277b01f1e090ff429986dffceb16d85aa045c
Thomas Mesquita <tmesquit@mines.edu>
2012-04-02 23:01:59 -0600

    added seed data for admin and member user

--- project06/db/seeds.rb
+++ project06/db/seeds.rb
@@ -1,3 +1,5 @@
-Role.create!(:name => 'developer', :description => 'The system developer. There is no higher access level.')
-Role.create!(:name => 'administrator', :description => 'A system administrator. Full access to all features.')
+Role.create!(:name => 'admin', :description => 'A system administrator. Full access to all features.')
 Role.create!(:name => 'member', :description => 'A member of the site')
+
+User.create(:username => 'admin', :password => 'password', :password_confirmation => 'password', :first_name => 'Adolf', :last_name => 'Coors', :email => 'admin@gamez.com', :role_id => Role.find_or_create_by_name("admin").id)
+User.create(:username => 'member', :password => 'password', :password_confirmation => 'password', :first_name => 'Matz', :last_name => 'Matsumoto', :email => 'member@gamez.com', :role_id => Role.find_or_create_by_name("member").id)
